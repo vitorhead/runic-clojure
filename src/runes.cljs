34093c10b2eb360d695354c11aad45b83e1273cd
vitorhead <vitorstipanich@hotmail.com>
2019-03-26 13:41:11 -0300

    Runic correction

--- src/runes.cljs
+++ src/runes.cljs
@@ -1,7 +1,7 @@
 (ns runes)
 
 ;https://www.furorteutonicus.eu/germanic/runescribe/index.php
-(def runes "ᚨᛒᚲᛞᛖᚠᚷᚺᛇᚲᛚᛗᚾᛟᛈᛩᚱᛋᛏᚢᚡᚹᛪᛃᛎ")
+(def runes "ᚨᛒᚲᛞᛖᚠᚷᚺᛇᚲᛚᛗᚾᛟᛈᛩᚱᛋᛏᚢᚡᚹᛪᛃᛋᛎ")
 (def alphabet "abcdefghijklmnopqrstuvwxyz")
 (def values (zipmap alphabet runes))
 
